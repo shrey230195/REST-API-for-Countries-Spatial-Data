f3f10775efb4b08e25ba5efcb5a2cd76f5b45989
shrey <shrey@rapido.bike>
2021-08-01 12:54:25 +0530

    add: custome serializer for neighbours, load data api

--- countries/views.py
+++ countries/views.py
@@ -2,11 +2,16 @@ from rest_framework import viewsets, status
 from django.shortcuts import render
 from rest_framework.response import Response
 from django.contrib.gis.db.models.functions import Distance
-from django.contrib.gis.geos import GEOSGeometry,Point
+from django.contrib.gis.geos import GEOSGeometry, MultiPolygon, Polygon
 from rest_framework.decorators import action
 from . models import  Countries
-from . serializers import CountriesSerializer
+from . serializers import CountriesSerializer, CountriesSerializerWithoutGeom
 from rest_framework.filters import SearchFilter
+import requests
+import json
+from django.db import IntegrityError
+from django.core.paginator import Paginator
+from rest_framework.pagination import PageNumberPagination
 
 
 class CountriesViewSet(viewsets.ModelViewSet):
@@ -14,3 +19,36 @@ class CountriesViewSet(viewsets.ModelViewSet):
 	queryset = Countries.objects.all()
 	filter_backends = [SearchFilter]
 	search_fields = ['$name']
+	pagination_class = PageNumberPagination
+
+	@action(detail=False, methods=['post'], serializer_class=CountriesSerializerWithoutGeom)
+	def get_neighbor_countries(self, request):
+		lookup_country = self.request.POST.get('name')
+		if not lookup_country:
+    			return Response({}, status=status.HTTP_200_OK)
+		countries = Countries.objects.filter(name=lookup_country)
+		neigbors = Countries.objects.filter(geom__touches=countries[0].geom)		
+		serialized = CountriesSerializerWithoutGeom(neigbors, many = True)
+		return Response(serialized.data, status=status.HTTP_200_OK)
+
+	@action(detail=False, methods=['get'])
+	def load_data(self, request):				
+		data = requests.get('https://datahub.io/core/geo-countries/r/0.geojson')
+		# print list of all resources:
+		json_data = data.json()				
+		for item in json_data["features"]:
+			geom_str = json.dumps(item["geometry"])
+			geom = GEOSGeometry(geom_str)
+			if isinstance(geom, Polygon):
+				geom = MultiPolygon([geom])
+			try:
+				Countries.objects.create(					
+					name=item["properties"]["ADMIN"],
+					geom=geom					
+				)
+			except IntegrityError as error:
+				print("entry already exists, skipping")
+				print(error)				
+		
+		print("addedd successfullly")
+		return Response({}, status=status.HTTP_200_OK)
--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -17,5 +17,6 @@ done
 
 python manage.py makemigrations && 
 python manage.py migrate && 
-python manage.py collectstatic --noinput --clear && 
+# python manage.py collectstatic --noinput --clear && 
+python manage.py collectstatic --noinput && 
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers=2
